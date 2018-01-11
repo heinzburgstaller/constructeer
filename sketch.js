775c5859697b9ebc8a0ff1f401794a5d0640a252
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2018-01-11 10:02:45 +0100

    I am the only motherfucker, who does anything?

--- sketch.js
+++ sketch.js
@@ -68,7 +68,7 @@ function mouseDragged() {
 
     var sorted = helper.sortTwoPoints(mousePressedX, mousePressedY, mouseX, mouseY);
     var calc = helper.doBasicCalculations(sorted.firstPoint, sorted.secondPoint);
-    drawingLegal = calc.c > 70 && calc.c <= 300;
+    drawingLegal = calc.c > 70 && calc.c <= 250;
   }
   checkMouseOnBody();
 }
