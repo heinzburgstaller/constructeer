74f75779c5345d0cf373b345f8f94b8a4d37f177
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2018-01-27 11:45:59 +0100

    Code formatted

--- elements/human.js
+++ elements/human.js
@@ -8,8 +8,8 @@ class Human {
     this.hitCallback = hitCallback;
 
     this.head_options = {
-      h: this.h/5,
-      w: this.h/5,
+      h: this.h / 5,
+      w: this.h / 5,
       friction: 0.95,
       restitution: 0.2,
       angle: 0,
@@ -19,12 +19,12 @@ class Human {
         group: "human"
       },
       chamfer: {
-            radius: this.h/10
+        radius: this.h / 10
       }
     }
     this.torso_options = {
-      h: 2*this.h/5,
-      w: 2*this.h/6,
+      h: 2 * this.h / 5,
+      w: 2 * this.h / 6,
       friction: 0.95,
       restitution: 0.2,
       angle: 0,
@@ -34,12 +34,12 @@ class Human {
         group: "human"
       },
       chamfer: {
-            radius: this.h/10
+        radius: this.h / 10
       }
     }
     this.left_arm_options = {
-      h: 2*this.h/5,
-      w: this.h/10,
+      h: 2 * this.h / 5,
+      w: this.h / 10,
       friction: 0.95,
       restitution: 0.2,
       angle: PI / 4, // a+0.785398,
@@ -49,12 +49,12 @@ class Human {
         group: "human"
       },
       chamfer: {
-            radius: this.h/10
+        radius: this.h / 10
       }
     }
     this.right_arm_options = {
-      h: 2*this.h/5,
-      w: this.h/10,
+      h: 2 * this.h / 5,
+      w: this.h / 10,
       friction: 0.95,
       restitution: 0.2,
       angle: -0.785398,
@@ -64,12 +64,12 @@ class Human {
         group: "human"
       },
       chamfer: {
-            radius: this.h/10
+        radius: this.h / 10
       }
     }
     this.left_foot_options = {
-      h: 2*this.h/5,
-      w: this.h/10,
+      h: 2 * this.h / 5,
+      w: this.h / 10,
       friction: 0.95,
       restitution: 0.2,
       angle: 0,
@@ -79,12 +79,12 @@ class Human {
         group: "human"
       },
       chamfer: {
-            radius: this.h/10
+        radius: this.h / 10
       }
     }
     this.right_foot_options = {
-      h: 2*this.h/5,
-      w: this.h/10,
+      h: 2 * this.h / 5,
+      w: this.h / 10,
       friction: 0.95,
       restitution: 0.2,
       angle: 0,
@@ -94,16 +94,16 @@ class Human {
         group: "human"
       },
       chamfer: {
-            radius: this.h/10
+        radius: this.h / 10
       }
     }
 
-    this.head = Bodies.rectangle(x, y-8.5*this.h/10, this.head_options.w, this.head_options.h, this.head_options);
-    this.torso = Bodies.rectangle(x, y-11*this.h/20, this.torso_options.w, this.torso_options.h, this.torso_options);
-    this.left_arm = Bodies.rectangle(x-2*this.h/10, y-3*this.h/5, this.left_arm_options.w, this.left_arm_options.h, this.left_arm_options);
-    this.right_arm = Bodies.rectangle(x+2*this.h/10, y-3*this.h/5, this.right_arm_options.w, this.right_arm_options.h, this.right_arm_options);
-    this.left_foot = Bodies.rectangle(x-this.h/10, y-this.h/5, this.left_foot_options.w, this.left_foot_options.h, this.left_foot_options);
-    this.right_foot = Bodies.rectangle(x+this.h/10, y-this.h/5, this.right_foot_options.w, this.right_foot_options.h, this.right_foot_options);
+    this.head = Bodies.rectangle(x, y - 8.5 * this.h / 10, this.head_options.w, this.head_options.h, this.head_options);
+    this.torso = Bodies.rectangle(x, y - 11 * this.h / 20, this.torso_options.w, this.torso_options.h, this.torso_options);
+    this.left_arm = Bodies.rectangle(x - 2 * this.h / 10, y - 3 * this.h / 5, this.left_arm_options.w, this.left_arm_options.h, this.left_arm_options);
+    this.right_arm = Bodies.rectangle(x + 2 * this.h / 10, y - 3 * this.h / 5, this.right_arm_options.w, this.right_arm_options.h, this.right_arm_options);
+    this.left_foot = Bodies.rectangle(x - this.h / 10, y - this.h / 5, this.left_foot_options.w, this.left_foot_options.h, this.left_foot_options);
+    this.right_foot = Bodies.rectangle(x + this.h / 10, y - this.h / 5, this.right_foot_options.w, this.right_foot_options.h, this.right_foot_options);
 
     World.add(world, this.head);
     World.add(world, this.torso);
@@ -121,128 +121,128 @@ class Human {
     this.head.onCollide(collide);
   }
 
-  testEvent(){
+  testEvent() {
     debugger;
   }
 
-  createConstraints(){
+  createConstraints() {
     var headTorso1 = Constraint.create({
-        bodyA: this.head,
-        length: 0,
-        pointA: {
-            x: -this.head_options.w/4,
-            y: this.head_options.h/2
-        },
-        pointB: {
-            x: -this.head_options.w/4,
-            y: -this.torso_options.h/2
-        },
-        bodyB: this.torso,
-        stiffness: 0.02
+      bodyA: this.head,
+      length: 0,
+      pointA: {
+        x: -this.head_options.w / 4,
+        y: this.head_options.h / 2
+      },
+      pointB: {
+        x: -this.head_options.w / 4,
+        y: -this.torso_options.h / 2
+      },
+      bodyB: this.torso,
+      stiffness: 0.02
     });
     var headTorso2 = Constraint.create({
-        bodyA: this.head,
-        length: 0,
-        pointA: {
-            x: this.head_options.w/4,
-            y: this.head_options.h/2
-        },
-        pointB: {
-            x: this.head_options.w/4,
-            y: -this.torso_options.h/2
-        },
-        bodyB: this.torso,
-        stiffness: 0.02
+      bodyA: this.head,
+      length: 0,
+      pointA: {
+        x: this.head_options.w / 4,
+        y: this.head_options.h / 2
+      },
+      pointB: {
+        x: this.head_options.w / 4,
+        y: -this.torso_options.h / 2
+      },
+      bodyB: this.torso,
+      stiffness: 0.02
     });
 
     var LeftArmTorso1 = Constraint.create({
-        bodyA: this.left_arm,
-        length: 0,
-        pointA: {
-            x: 0,
-            y: -this.left_arm_options.h/2
-        },
-        pointB: {
-            x: -this.torso_options.w/1.6,
-            y: -this.torso_options.h/1.6
-        },
-        bodyB: this.torso,
-        stiffness: 0.02
+      bodyA: this.left_arm,
+      length: 0,
+      pointA: {
+        x: 0,
+        y: -this.left_arm_options.h / 2
+      },
+      pointB: {
+        x: -this.torso_options.w / 1.6,
+        y: -this.torso_options.h / 1.6
+      },
+      bodyB: this.torso,
+      stiffness: 0.02
     });
 
     var RightArmTorso1 = Constraint.create({
-        bodyA: this.right_arm,
-        length: 0,
-        pointA: {
-            x: 0,
-            y: -this.right_arm_options.h/2
-        },
-        pointB: {
-            x: +this.torso_options.w/1.6,
-            y: -this.torso_options.h/1.6
-        },
-        bodyB: this.torso,
-        stiffness: 0.02
+      bodyA: this.right_arm,
+      length: 0,
+      pointA: {
+        x: 0,
+        y: -this.right_arm_options.h / 2
+      },
+      pointB: {
+        x: +this.torso_options.w / 1.6,
+        y: -this.torso_options.h / 1.6
+      },
+      bodyB: this.torso,
+      stiffness: 0.02
     });
 
     var LeftFootTorso1 = Constraint.create({
-        bodyA: this.torso,
-        length: 0,
-        pointA: {
-            x: -this.h/10,
-            y: (11*this.h/20 - this.left_foot_options.h)
-        },
-        pointB: {
-            x: 0,
-            y: -this.left_foot_options.h/2
-        },
-        bodyB: this.left_foot,
-        stiffness: 0.6
+      bodyA: this.torso,
+      length: 0,
+      pointA: {
+        x: -this.h / 10,
+        y: (11 * this.h / 20 - this.left_foot_options.h)
+      },
+      pointB: {
+        x: 0,
+        y: -this.left_foot_options.h / 2
+      },
+      bodyB: this.left_foot,
+      stiffness: 0.6
     });
 
     var LeftFootTorso2 = Constraint.create({
-        bodyA: this.torso,
-        length: 0,
-        pointA: {
-            x: -this.h/10,
-            y: (11*this.h/20 - this.left_foot_options.h)+10
-        },
-        pointB: {
-            x: 0,
-            y: -this.left_foot_options.h/2 + 10
-        },
-        bodyB: this.left_foot,
-        stiffness: 0.6
+      bodyA: this.torso,
+      length: 0,
+      pointA: {
+        x: -this.h / 10,
+        y: (11 * this.h / 20 - this.left_foot_options.h) + 10
+      },
+      pointB: {
+        x: 0,
+        y: -this.left_foot_options.h / 2 + 10
+      },
+      bodyB: this.left_foot,
+      stiffness: 0.6
     });
 
     var RightFootTorso1 = Constraint.create({
-        bodyA: this.torso,
-        length: 0,
-        pointA: {
-            x: +this.h/10,
-            y: (11*this.h/20 - this.left_foot_options.h)
-        },
-        pointB: {
-            x: 0,
-            y: -this.left_foot_options.h/2
-        },
-        bodyB: this.right_foot,
-        stiffness: 0.6
+      bodyA: this.torso,
+      length: 0,
+      pointA: {
+        x: +this.h / 10,
+        y: (11 * this.h / 20 - this.left_foot_options.h)
+      },
+      pointB: {
+        x: 0,
+        y: -this.left_foot_options.h / 2
+      },
+      bodyB: this.right_foot,
+      stiffness: 0.6
     });
 
     var RightFootTorso2 = Constraint.create({
-        bodyA: this.torso,
-        length: 0,
-        pointA: {
-            x: +this.h/10,
-            y:(11*this.h/20 - this.left_foot_options.h) + 10
-        },
-        pointB: {
-            x: 0,
-            y: -this.left_foot_options.h/2 + 10
-        },
-        bodyB: this.right_foot,
-        stiffness: 0.6
+      bodyA: this.torso,
+      length: 0,
+      pointA: {
+        x: +this.h / 10,
+        y: (11 * this.h / 20 - this.left_foot_options.h) + 10
+      },
+      pointB: {
+        x: 0,
+        y: -this.left_foot_options.h / 2 + 10
+      },
+      bodyB: this.right_foot,
+      stiffness: 0.6
     });
 
     World.add(world, headTorso1);
@@ -264,7 +264,7 @@ class Human {
     this.drawBodypart(this.left_arm, this.left_arm_options, '#d7d73c');
   }
 
-  remove(){
+  remove() {
     World.remove(world, this.head);
     World.remove(world, this.right_arm);
     World.remove(world, this.left_foot);
@@ -273,7 +273,7 @@ class Human {
     World.remove(world, this.left_arm);
   }
 
-  drawBodypart(part, options, color){
+  drawBodypart(part, options, color) {
     push();
     translate(part.position.x, part.position.y);
     rotate(part.angle);
@@ -281,7 +281,7 @@ class Human {
     strokeWeight(1);
     stroke('#202021');
     fill(color);
-    rect(0, 0, options.w, options.h,this.h/10);
+    rect(0, 0, options.w, options.h, this.h / 10);
     pop();
   }
 
